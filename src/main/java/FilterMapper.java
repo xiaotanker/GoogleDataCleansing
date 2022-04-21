27522c27a347fbba2412c40e875475c162fb67a7
tianbojiang <tj2099@nyu.edu>
2022-04-20 22:49:26 -0400

    filter only county data

--- src/main/java/FilterMapper.java
+++ src/main/java/FilterMapper.java
@@ -4,7 +4,6 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
-import java.time.LocalDate;
 
 public class FilterMapper
     extends Mapper<LongWritable, Text, Text, Text>{
@@ -18,7 +17,7 @@ public class FilterMapper
         String[] split = line.split(",");
         split[14] = split[14].substring(0,split[14].length()-1);
 
-        if(split[0].equals("US")) {
+        if(split[0].equals("US")&&split[3].length()>0) {
             String output = String.join(",",split[1],split[2],split[3],split[7],split[8],split[9],split[10],split[11],split[12],split[13],split[14]);
             context.write(new Text(split[7]), new Text(output));
         }
